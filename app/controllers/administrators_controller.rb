cc0ea2b47c057101694f904eea93a33708426ce4
Spencer Plant <splant.10@gmail.com>
2020-05-09 12:19:37 -0400

    Populate administrators table by seed

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,5 +1,6 @@
 class AdministratorsController < ApplicationController
   skip_before_action :check_authorized, only: [:new, :create]
+  before_action :check_environment
 
   def new
     @administrator = Administrator.new
@@ -9,5 +10,11 @@ class AdministratorsController < ApplicationController
     @administrator = Administrator.create(params.require(:administrator).permit(:username, :password))
     session[:administrator_id] = @administrator.id
     redirect_to '/welcome'
+  end
+
+  private
+
+  def check_environment
+    raise ActionController::RoutingError.new("Not found") unless Rails.env.development?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Admins
+[
+  { username: "splantio", password_digest: "$2a$12$kGIyW2fX9dyTjOF6mb2JYucRyuyLlaaAuCmZn9jK7oQ230FSKvJIi" },
+].each do |admin_data|
+  next if Administrator.find_by(username: admin_data[:username]).present?
+  Administrator.create(username:  admin_data[:username], password_digest: admin_data[:password_digest])
+end
+
+# Houses
+%w(
+  Gryffindor
+  Slytherin
+  Hufflepuff
+  Ravenclaw
+).each do |name|
+  next if House.find_by(name: name).present?
+  House.create(name: name)
+end
